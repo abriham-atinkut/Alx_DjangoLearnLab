f1f07f4f33cc61fb32a833a02c111ac4604d721f
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-01-11 20:04:12 +0300

    add filter and search capabilities

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Book
 
-admin.site.register(Book)
+class MyModelAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author', 'publication_year') # Add a search bar
+    list_filter = ('title', 'author', 'publication_year')  # Add filter options
+
+admin.site.register(Book, MyModelAdmin)
