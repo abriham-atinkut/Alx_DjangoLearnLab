8ed01d5412ab80b45c11ec321d69fca08dfeebbf
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-01-23 18:01:15 +0300

    add some string

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -34,6 +34,7 @@ def list_books():
     book.all()
     ["Library.objects.get(name=library_name)", "books.all()"]
     ["Author.objects.get(name=author_name)", "objects.filter(author=author)"]
+    ["Librarian.objects.get(library="]
 
 
 # from relationship_app.query_samples import librarian_info
