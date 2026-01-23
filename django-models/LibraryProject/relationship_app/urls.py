cadf7ef249340e610ba5a17e8afe79ba215bf111
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-01-23 21:55:01 +0300

    create 3 html file and add one directory fix #4

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import (
+    list_books,
+    LibraryDetailView,
+    UserLoginView,
+    UserLogoutView,
+    register
+)
 
 app_name = 'relationship_app'
 
 urlpatterns = [
+    # existing views
     path('books/', list_books, name='list_books'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+    # authentication
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
-
 from .models import Library, Book
 
-
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
 
 def list_books(request):
     books = Book.objects.select_related('author').all()
@@ -17,3 +19,30 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
     Book.objects.all()
+
+
+
+# Login view (built-in)
+class UserLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+
+# Logout view (built-in)
+class UserLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+
+# Registration view
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('relationship_app:list_books')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'relationship_app/register.html', {
+        'form': form
+    })
