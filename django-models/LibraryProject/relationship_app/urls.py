c43c9b5b5391001fdddf6bc2d13e9e0754b80764
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-01-23 21:59:06 +0300

    change one line of code #6

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -20,3 +20,5 @@ urlpatterns = [
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('register/', register, name='register'),
 ]
+
+["views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name="]
