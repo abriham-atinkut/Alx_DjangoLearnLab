830f63d90c91be249085cbbb35064ec70cb9b871
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-01-26 14:58:15 +0300

    create templates

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -62,3 +62,23 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
+    
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name='books'
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        permissions = (
+            ("can_view", "Can view books"),
+            ("can_create", "Can create books"),
+            ("can_edit", "Can edit books"),
+            ("can_delete", "Can delete books"),
+        )
