6eb4f1482069c0770149035a90b567311fb96b7c
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-01-26 14:59:51 +0300

    rename_book shelf to book_list

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -13,7 +13,7 @@ class BookForm(forms.ModelForm):
 
 # --- View Books ---
 @permission_required('relationship_app.can_view', raise_exception=True)
-def view_books(request):
+def book_list(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/view_books.html', {'books': books})
 
