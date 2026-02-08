0779c92dba619cc259dd949e51d9761da494b115
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-02-09 01:06:13 +0300

    impleiment auth and premissions in django rest framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, permissions
 from .models import Book
 from .serializers import BookSerializer
 
@@ -11,4 +10,6 @@ class BookViewSet(viewsets.ModelViewSet):
     # viewSet that provides full CRUD operations for Book
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    # Only authenticated users can access
+    permission_classes = [permissions.IsAuthenticated]
 
