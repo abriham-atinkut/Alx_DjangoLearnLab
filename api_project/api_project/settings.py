be9d8b3e6cdfbdedb1fa03e8f18d33b4eaa2981c
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-02-09 01:10:44 +0300

    fix: add one line of comment in setting

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+# rest_framework.permissions.IsAuthenticated
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
