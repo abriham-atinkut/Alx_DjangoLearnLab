32b68ea705bdfae319ae2512df2790848e9e4e17
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-02-09 00:07:54 +0300

    By using modelviewsSet and route implementing CRUD operations

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    # viewSet that provides full CRUD operations for Book
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
