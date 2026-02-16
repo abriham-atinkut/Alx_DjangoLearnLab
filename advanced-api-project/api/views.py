50664f2e9710a9fdb5c351d5856ee26973228317
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-02-16 15:49:59 +0300

    add: one line of comment in views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, permissions, filters
 from .models import Book
 from .serializers import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-
+# from django_filters import rest_framework
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
