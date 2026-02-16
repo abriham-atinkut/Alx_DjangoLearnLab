8b38e1b5472985dcd900585f7e19a6ad7e51720b
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-02-16 15:48:03 +0300

    implementing filtering, searching and ordering

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,33 +1,13 @@
-from rest_framework import generics, permissions
-from .models import Book
-from .serializers import BookSerializer
+from django.urls import path
+from .views import (
+    BookListView, BookDetailView,
+    BookCreateView, BookUpdateView, BookDeleteView
+)
 
-
-class BookListView(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny] 
-
-class BookDetailView(generics.RetrieveAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]
-
-
-class BookCreateView(generics.CreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated] 
-
-
-class BookUpdateView(generics.UpdateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class BookDeleteView(generics.DestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-# ["books", "books/create", "books/update", "books/delete"]
+urlpatterns = [
+    path("books/", BookListView.as_view(), name="book-list"),
+    path("books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),
+    path("books/create/", BookCreateView.as_view(), name="book-create"),
+    path("books/<int:pk>/update/", BookUpdateView.as_view(), name="book-update"),
+    path("books/<int:pk>/delete/", BookDeleteView.as_view(), name="book-delete"),
+]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,22 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from .models import Book
 from .serializers import BookSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+
+    filterset_fields = ['title', 'author__name', 'publication_year']
+
+    search_fields = ['title', 'author__name']
+
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
